79453edbb625e6824ef988898ab9e5b193d4008f
Yusuke Kido <yusuke.kido@legalscape.co.jp>
2020-12-18 23:00:38 +0900

    v1.0.0

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,52 +1,107 @@
-<script>
-  import successkid from 'images/successkid.jpg';
-</script>
+<script lang="ts">
+  import { onMount } from 'svelte';
 
-<style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
+  import { fade } from 'svelte/transition';
+  import { A } from '../data/a';
+  import { B } from '../data/b';
+
+  let laws: string[] = [];
+  let interval = 50;
+  let url = '';
+  let dialog = false;
+
+  function pick<T>(array: Array<T>) {
+    return array[Math.floor(Math.random() * array.length)];
   }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
+  function generate() {
+    const i = laws.length;
+    const newLaw = pick(A) + 'は、' + pick(B) + '。';
+    laws.push('');
 
-  figure {
-    margin: 0 0 1em 0;
-  }
+    for (let j = 1; j <= newLaw.length; ++j) {
+      setTimeout(() => {
+        laws[i] = newLaw.slice(0, j);
+        laws = laws;
+      }, j * 100);
+    }
 
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+    setTimeout(generate, interval);
+    if (interval < 3000) {
+      interval *= 1.2;
     }
   }
+
+  onMount(() => {
+    generate();
+    url = location.href;
+  });
+</script>
+
+<style lang="sass">
+  article
+    font-family: serif
+
+    @media (min-width: 992px)
+      writing-mode: vertical-rl
+
+  .law
+    margin: 1em
+
+    user-select: all
+
+    b
+      span
+        text-combine-upright: all
+
+    i
+      font-size: 0
+
+  .btn-about
+    position: fixed
+    bottom: 1em
+    left: 1em
+    border: 0
+    background: #ffffffe5
+    color: #4444ed
+    cursor: pointer
+
+  nav
+    position: fixed
+    top: 50vh
+    left: 50vw
+    padding: 2em
+    min-width: 320px
+    background: #fcfdfddd
+    box-shadow: 0 0 3px #00000044
+    transform: translate(-50%, -50%)
+
+    .right
+      float: right
+      font-size: 0.5em
+
+    img
+      width: 4em
+
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>#ない法律</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<article>
+  {#each laws as law, number}
+    <div class="law" transition:fade><b>第<span>{number + 1}</span>条　</b>{law}<i>　#ない法律 {url}</i></div>
+  {/each}
+</article>
 
-<figure>
-  <img alt="Success Kid" src={successkid} />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<button class="btn-about" on:click={() => (dialog = !dialog)}>これは何</button>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+{#if dialog}
+  <nav transition:fade>
+    <p>日本の法令を解析して、「Aは、B。」という形の条文を適当に集め、適当につなぎかえました。</p>
+    <p class="right"><a href="https://twitter.com/p_km">私が作りました <img src="me.svg" /></a></p>
+    <p><a href="https://github.com/kissge/legaltech">source</a></p>
+    <button on:click={() => (dialog = !dialog)}>とじる</button>
+  </nav>
+{/if}
