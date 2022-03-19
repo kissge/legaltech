3671a060c5208953deec7a49d16c8f91571fe771
Yusuke Kido <mail@yusuk.eki.do>
2022-03-19 22:50:03 +0900

    PWA A2HS対応をしようとしたらいろいろ大変なことになった

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,10 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-  .use('/legaltech', compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
-  .listen(PORT, (err) => {
+polka({
+  onError(err) {
     if (err) console.log('error', err);
-  });
+  },
+}) // You can also use Express
+  .use('/legaltech', compression({ threshold: 0 }) as any /** ?????? */, sirv('static', { dev }), sapper.middleware())
+  .listen(PORT);
